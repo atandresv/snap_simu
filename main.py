cc6e2ede9297c19a360a49ed7a4a8d61d534c7d6
atandresv <andresv@alvotech.com>
2025-03-17 10:00:30 +0000

    Reading outputs added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from os import read
 import logging_config
 from siemens_comm import Plc
 import time
-
 
 def main() -> None:
     plc: Plc = Plc()  # Assuming you need to initialize the Plc object
@@ -10,7 +10,9 @@ def main() -> None:
         while True:
             # Perform some operations with plc
             # Example: plc.read_data() or plc.write_data()
-            
+            km1_coil: bool = plc.read_output(0, 0)
+            km2_coil: bool = plc.read_output(0, 1)
+            print(km1_coil, km2_coil)
             time.sleep(1)
     except KeyboardInterrupt:
         print("Interrupted by user, shutting down.")
