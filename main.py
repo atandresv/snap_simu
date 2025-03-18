ab4f1e083c8c67604f96248fe31c69a559f11f92
atandresv <andresv@alvotech.com>
2025-03-18 11:09:38 +0000

    Logging config added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from siemens_comm import Plc
 from system_simu import Contactor
 import time
 
+# Configure logging
+setup_logging()
+
 def main() -> None:
     plc: Plc = Plc()  # Assuming you need to initialize the Plc object
 
